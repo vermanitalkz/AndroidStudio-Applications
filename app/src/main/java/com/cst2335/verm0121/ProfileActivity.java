a8d947b57f3499419a18ef75c397d208fb9780fb
Vianshu Vermani <verm0121@algonquinlive.com>
2023-02-01 15:43:48 -0500

    LAB3 start - added some files

---
+++ app/src/main/java/com/cst2335/verm0121/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.cst2335.verm0121;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}

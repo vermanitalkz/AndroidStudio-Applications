22bce9a5ef177ba1109e7a928c104f0d3f9ccec7
Vianshu Vermani <verm0121@algonquinlive.com>
2023-02-12 10:16:18 -0500

    LAB3 Completed

--- app/src/main/java/com/cst2335/verm0121/MainActivity.java
+++ app/src/main/java/com/cst2335/verm0121/MainActivity.java
@@ -1,14 +1,34 @@
 package com.cst2335.verm0121;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button takePictureButton = findViewById(R.id.button2);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = findViewById(R.id.email);
+
+                Intent goProfile = new Intent(MainActivity.this, ProfileActivity.class);
+                goProfile.putExtra("EMAIL", emailEditText.getText().toString());
+                startActivity(goProfile);
+            }
+        });
+
     }
-}
+    }
--- app/src/main/java/com/cst2335/verm0121/ProfileActivity.java
+++ app/src/main/java/com/cst2335/verm0121/ProfileActivity.java
@@ -1,17 +1,96 @@
 package com.cst2335.verm0121;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private static final String TAG = "ProfileActivity";
+    private ImageView ViewImg;
+    private ActivityResultLauncher<Intent> myPictureTakerLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        Log.e(TAG, "In function: onCreate");
+        ViewImg = findViewById(R.id.takePicture);
+        Button takePictureButton = findViewById(R.id.button);
+        
+        takePictureButton.setOnClickListener(v -> dispatchTakePictureIntent());
+
+        myPictureTakerLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        ImageView ViewImg = findViewById(R.id.takePicture);
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            assert data != null;
+                            Bitmap imgBitmap = (Bitmap) data.getExtras().get("data");
+                            ViewImg.setImageBitmap(imgBitmap);
+                        } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                            Log.i(TAG, "User refused to capture a picture.");
+                           }
+                    }
+                });
+        };
+    private void dispatchTakePictureIntent() {
+        Intent PictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (PictureIntent.resolveActivity(getPackageManager()) != null) {
+            myPictureTakerLauncher.launch(PictureIntent);
+        }
+        EditText namimg = findViewById(R.id.enterName);
+        String name = "Vianshu";
+        Intent MainForm = getIntent();
+        String email = MainForm.getStringExtra("EMAIL");
+        TextView Textemail = findViewById(R.id.editTextTextPersonName5);
+        Textemail.setText(email);
+        namimg.setText(name);
     }
-}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "In function: onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "In function: onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "In function: onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "In function: onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "In function: onDestroy");
+    }
+
+
+    }
